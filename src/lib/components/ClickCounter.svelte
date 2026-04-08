91fb452596deecd1648d1a1331acc5cd5dd0a65a
SimoneBusato-dot <simone.busato@mail.polimi.it>
2026-04-08 15:49:32 +0200

    utilizzo state e props

---
+++ src/lib/components/ClickCounter.svelte
@@ -0,0 +1,12 @@
+<script>
+    let props = $props()
+    let count = $state(props.initialCount || 0)
+    let double = $derived(count*2)
+</script>
+
+<button onclick={() => count++}>
+    Clicks: {count}
+</button>
+<span> doubled is {double}</span>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import ClickCounter from "$lib/components/ClickCounter.svelte";
+</script>
+
+<ClickCounter initialCount=2/>
