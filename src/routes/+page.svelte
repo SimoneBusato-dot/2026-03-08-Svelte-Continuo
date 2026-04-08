55f7694dfb425989a2619875e567a19bc45e170b
SimoneBusato-dot <simone.busato@mail.polimi.it>
2026-04-08 17:29:12 +0200

    effect e on mount

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
     import ClickCounter from "$lib/components/ClickCounter.svelte";
+    import CanvasSquare from "$lib/components/CanvasSquare.svelte";
 
     let count = $state(5)
 </script>
 
 <ClickCounter bind:initialCount={count}/>
-
 <p>count form parent is {count}</p>
+<CanvasSquare />
+
